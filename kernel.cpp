496197346c47408a8daf60186144742075c11630
kinvy66 <kinvy66@163.com>
2021-08-18 18:15:01 +0800

    run ok(gdt)

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,9 @@
+#include "types.h"
+#include "gdt.h"
+
 void printf(char* str)
 {
-    unsigned short*  VideoMemory = (unsigned short*) 0xb8000;
+    static uint16_t*  VideoMemory = (uint16_t*) 0xb8000;
 
     for (int i = 0; str[i] != '\0'; ++i) 
     {
@@ -20,12 +23,13 @@ extern "C" void callConstructors()
     }
 }
 
-
-
-
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-    printf((char*)"Hello World!\n");
+    printf((char*)"Hello World!");
+
+    GlobalDescriptorTable gdt;
+    
+
 
     while (1);
 }
