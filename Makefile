c87c7ae8b8901b836bc5c57947aa8a7cb6b35598
kinvy66 <kinvy66@163.com>
2021-08-19 16:43:31 +0800

    port ok

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o kernel.o
+objects = loader.o gdt.o port.o kernel.o
 
 %.o: %.cpp
 	g++ $(GPPPARAMS) -o $@ -c $<
@@ -40,7 +40,6 @@ run: mykernel.iso
 	(killall VirtualBox && sleep 1) || true
 	VirtualBox --startvm "My Operating System" &
 	
-
-clear: 
-	rm *.o
-	rm *.bin
+.PHONY: clean
+clean: 
+	rm -f $(objects) mykernel.bin
--- kernel.cpp
+++ kernel.cpp
@@ -3,11 +3,44 @@
 
 void printf(char* str)
 {
-    static uint16_t*  VideoMemory = (uint16_t*) 0xb8000;
+    static uint16_t*  VideoMemory = (uint16_t*) 0xb8000;  //0xB8000~0xBFFFF是显存在内存上的映射地址
+
+    static uint8_t x = 0, y = 0;
 
     for (int i = 0; str[i] != '\0'; ++i) 
     {
-        VideoMemory[i] = (VideoMemory [i] & 0xFF00) | str[i] ;
+        switch (str[i])
+        {
+        case '\n':
+            y++;
+            x =0;
+            break;
+
+        default:
+            VideoMemory[80*y+x] = (VideoMemory [80*y+x] & 0xFF00) | str[i] ;
+            x++;
+            break;
+        }
+        
+
+        if(x >= 80)
+        {
+            y++;
+            x = 0;
+        }
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+            {
+                for(x = 0; x < 80; x++)
+                {
+                    VideoMemory[80*y+x] = (VideoMemory [80*y+x] & 0xFF00) | ' ' ;
+                }
+                
+            }
+            x = 0;
+            y = 0;
+        }
     }
 }
 
@@ -27,6 +60,9 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf((char*)"Hello World!");
 
+    printf((char*)"Hello World!22\n");
+
+    printf((char*)"Hello World!3333\n");
     GlobalDescriptorTable gdt;
     
 
